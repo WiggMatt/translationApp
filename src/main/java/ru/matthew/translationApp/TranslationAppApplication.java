c715aebb40b63ccb74289cbbdeb3475be30ee4c5
Matthew <defender_0000@mail.ru>
2024-08-04 12:17:35 +0300

    Initial commit

---
+++ src/main/java/ru/matthew/translationApp/TranslationAppApplication.java
@@ -0,0 +1,13 @@
+package ru.matthew.translationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslationAppApplication.class, args);
+	}
+
+}
